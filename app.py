99fe3701e75da2356c3bbdd918ebad7f9cec941e
“chongweibing” <1502534I@student.tp.edu.sg>
2017-06-14 10:10:50 +0800

    show all flows in cli

--- app.py
+++ app.py
@@ -13,5 +13,12 @@ flows = [
         }
 ]
 
+#Show all flows in cli
+@app.route('/todo/api/v1.0/readall/flowscli', methods=['GET'])
+def get_flowscli():
+        p = subprocess.Popen(["ovs-ofctl", "dump-flows", "s1"], stdout=subprocess.PIPE)
+        output, err = p.communicate()
+        return output + "\n"
+
 if __name__ == '__main__':
         app.run(debug=True)
